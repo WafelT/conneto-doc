b984176d6cc9e9417c8d666976db007b50d4c061
Dmitrii Turcanu <dumitru_turcanu@alsodev.md>
2023-08-03 10:01:41 +0300

    [update] adding field working with languages files

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,62 +1,65 @@
-document.querySelector('#codeMapGetters').value = 
-`import { mapGetters } from 'vuex'
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (function() { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
 
-computed: {
-    ...mapGetters({
-        exampleData: 'example/exampleData',
-        exampleDataSecond: 'example/exampleDataSecond',
-    })
-}`
-document.querySelector('#codeMapAction').value = 
-`this.$store.dispatch('example/exampleActionFunction', data)`
-document.querySelector('#codeStoreMutation').value = 
-`mutations: {
-    SET_EXAMPLE_DATA: (state, data) => state.exampleData = data,
-},`
-document.querySelector('#codeComponentImports').value = 
-`components: {
-    HeaderDeleted: () => import('~/components/common/the-header/HeaderDeleted.vue'),
-    
-    ContractsIcon: () => import('~/components/common/icons/ContractsIcon.vue'),
-    DeletedFilesIcon: () => import('~/components/common/icons/DeletedFilesIcon.vue'),
-    FolderIcon: () => import('~/components/common/icons/FolderIcon.vue'),
-    ClipIcon: () => import('~/components/common/icons/ClipIcon.vue'),
+/***/ "./src/assets/js/app.js":
+/*!******************************!*\
+  !*** ./src/assets/js/app.js ***!
+  \******************************/
+/***/ (function() {
 
-    SkeletonProjectsFolder: () => import('~/components/common/skeletons/SkeletonProjectsFolder.vue'),
-},`
-const tels = document.querySelectorAll("input[type=\"tel\"]");
+eval("document.querySelector('#codeMapGetters').value = \r\n`import { mapGetters } from 'vuex'\r\n\r\ncomputed: {\r\n    ...mapGetters({\r\n        exampleData: 'example/exampleData',\r\n        exampleDataSecond: 'example/exampleDataSecond',\r\n    })\r\n}`\r\ndocument.querySelector('#codeMapAction').value = \r\n`this.$store.dispatch('example/exampleActionFunction', data)`\r\ndocument.querySelector('#codeStoreMutation').value = \r\n`mutations: {\r\n    SET_EXAMPLE_DATA: (state, data) => state.exampleData = data,\r\n},`\r\ndocument.querySelector('#codeComponentImports').value = \r\n`components: {\r\n    HeaderDeleted: () => import('~/components/common/the-header/HeaderDeleted.vue'),\r\n    \r\n    ContractsIcon: () => import('~/components/common/icons/ContractsIcon.vue'),\r\n    DeletedFilesIcon: () => import('~/components/common/icons/DeletedFilesIcon.vue'),\r\n    FolderIcon: () => import('~/components/common/icons/FolderIcon.vue'),\r\n    ClipIcon: () => import('~/components/common/icons/ClipIcon.vue'),\r\n\r\n    SkeletonProjectsFolder: () => import('~/components/common/skeletons/SkeletonProjectsFolder.vue'),\r\n},`\r\ndocument.querySelector('#codeLanguageImport').value = \r\n`export default {\r\n    contractSelectHeaderCategory: 'Category',\r\n},`\n\n//# sourceURL=webpack://wafelt/./src/assets/js/app.js?");
 
-if (tels) {
-    tels.forEach(el => {
-        IMask(el, { mask: "+ {0} (000) 000 00 000 00" });
-    });
-}
-const observer = lozad('.lozad', {
-    loaded: function(img) {
-        img.addEventListener('load', function() {
-            img.classList.add('loaded');
-        });
-        
-        if(img.hasAttribute('data-background-image')) {
-            img.classList.add('loaded');
-        };
-    },
-});
+/***/ }),
 
+/***/ "./src/assets/js/imask.js":
+/*!********************************!*\
+  !*** ./src/assets/js/imask.js ***!
+  \********************************/
+/***/ (function() {
 
-observer.observe();
-const smoothLinks = document.querySelectorAll('a[href^="#"]');
-if (smoothLinks) {
-    for (let smoothLink of smoothLinks) {
-        smoothLink.addEventListener('click', function (e) {
-            e.preventDefault();
-            const id = smoothLink.getAttribute('href');
-    
-            if (id === "#") return
-            document.querySelector(id).scrollIntoView({
-                behavior: 'smooth',
-                block: 'start'
-            });
-        });
-    };
-}
+eval("const tels = document.querySelectorAll(\"input[type=\\\"tel\\\"]\");\r\n\r\nif (tels) {\r\n    tels.forEach(el => {\r\n        IMask(el, { mask: \"+ {0} (000) 000 00 000 00\" });\r\n    });\r\n}\n\n//# sourceURL=webpack://wafelt/./src/assets/js/imask.js?");
+
+/***/ }),
+
+/***/ "./src/assets/js/lozad.js":
+/*!********************************!*\
+  !*** ./src/assets/js/lozad.js ***!
+  \********************************/
+/***/ (function() {
+
+eval("const observer = lozad('.lozad', {\r\n    loaded: function(img) {\r\n        img.addEventListener('load', function() {\r\n            img.classList.add('loaded');\r\n        });\r\n        \r\n        if(img.hasAttribute('data-background-image')) {\r\n            img.classList.add('loaded');\r\n        };\r\n    },\r\n});\r\n\r\n\r\nobserver.observe();\n\n//# sourceURL=webpack://wafelt/./src/assets/js/lozad.js?");
+
+/***/ }),
+
+/***/ "./src/assets/js/smooth-links.js":
+/*!***************************************!*\
+  !*** ./src/assets/js/smooth-links.js ***!
+  \***************************************/
+/***/ (function() {
+
+eval("const smoothLinks = document.querySelectorAll('a[href^=\"#\"]');\r\nif (smoothLinks) {\r\n    for (let smoothLink of smoothLinks) {\r\n        smoothLink.addEventListener('click', function (e) {\r\n            e.preventDefault();\r\n            const id = smoothLink.getAttribute('href');\r\n    \r\n            if (id === \"#\") return\r\n            document.querySelector(id).scrollIntoView({\r\n                behavior: 'smooth',\r\n                block: 'start'\r\n            });\r\n        });\r\n    };\r\n}\n\n//# sourceURL=webpack://wafelt/./src/assets/js/smooth-links.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	__webpack_modules__["./src/assets/js/app.js"]();
+/******/ 	__webpack_modules__["./src/assets/js/imask.js"]();
+/******/ 	__webpack_modules__["./src/assets/js/lozad.js"]();
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/assets/js/smooth-links.js"]();
+/******/ 	
+/******/ })()
+;
